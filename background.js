e28b62c30936414d600f3d039947eb2a39e0ad1d
Malay Hazarika <malay.hazarika@goswift.in>
2021-09-19 21:45:01 +0530

    Update installation script

--- background.js
+++ background.js
@@ -2,23 +2,31 @@
 
 // Installing HabitX
 chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        console.log("Installing Habitx....");
-
-    }else if(details.reason == "update"){
-        console.log("Updating HabitX...")
-    }
-
     const block_list = [
         { url : 'youtube.com'},
         { url : 'reddit.com'},
-        { url : 'twitter.com'},
-        { url : 'guthib.com'}
+        { url : 'twitter.com'}
     ]
 
-    chrome.storage.sync.set({
-        'habitxIsEnabled': true,
-        'habitxBlockCountdownSeconds':1,
-        'habitxBlockedSites': block_list
-    });
+    if(details.reason == "install"){
+        console.log("Installing Habitx....");
+        
+        chrome.storage.sync.set({
+            'habitxIsEnabled': true,
+            'habitxBlockCountdownSeconds':10,
+            'habitxBlockedSites': block_list
+        });
+
+    } else if (details.reason == "update"){
+        console.log("Updating HabitX...");
+
+        chrome.storage.sync.get(['habitxIsEnabled','habitxBlockCountdownSeconds','habitxBlockedSites'], (data) => {
+            chrome.storage.sync.set({
+                'habitxIsEnabled': true,
+                'habitxBlockCountdownSeconds': data.habitxBlockCountdownSeconds ? data.habitxBlockCountdownSeconds : 10,
+                'habitxBlockedSites': data.habitxBlockedSites ? data.habitxBlockedSites : block_list 
+            });
+        });
+
+    }
 });
