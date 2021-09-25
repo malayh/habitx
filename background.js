edae94f479a37cae2a94d1d317b295cf12c6ef44
Malay Hazarika <malay.hazarika@goswift.in>
2021-09-25 23:06:02 +0530

    Refactored installation, added new cofig

--- background.js
+++ background.js
@@ -1,31 +1,35 @@
-
-
-// Installing HabitX
-chrome.runtime.onInstalled.addListener(function(details){
-    const block_list = [
+const default_conf = {
+    habitxIsEnabled: true,
+    habitxBlockCountdownSeconds : 10,
+    habitxInteruptAfterMinutes : 5,
+    habitxReenableAfterMinutes : 5,
+    habitxBlockedSites : [
         { url : 'youtube.com'},
         { url : 'reddit.com'},
         { url : 'twitter.com'}
     ]
+}
+
+// Installing HabitX
+chrome.runtime.onInstalled.addListener(function(details){
 
     if(details.reason == "install"){
         console.log("Installing Habitx....");
         
-        chrome.storage.sync.set({
-            'habitxIsEnabled': true,
-            'habitxBlockCountdownSeconds':10,
-            'habitxBlockedSites': block_list
-        });
+        chrome.storage.sync.set(default_conf);
 
     } else if (details.reason == "update"){
         console.log("Updating HabitX...");
 
-        chrome.storage.sync.get(['habitxIsEnabled','habitxBlockCountdownSeconds','habitxBlockedSites'], (data) => {
-            chrome.storage.sync.set({
-                'habitxIsEnabled': true,
-                'habitxBlockCountdownSeconds': data.habitxBlockCountdownSeconds ? data.habitxBlockCountdownSeconds : 10,
-                'habitxBlockedSites': data.habitxBlockedSites ? data.habitxBlockedSites : block_list 
-            });
+        chrome.storage.sync.get(Object.keys(default_conf), (data) => {
+
+            let _obj = {};
+            for(const key of Object.keys(default_conf)) {
+                _obj[key] = data[key] ? data[key] : default_conf[key];
+            }
+
+            console.log(_obj);
+            chrome.storage.sync.set(_obj);
         });
 
     }
